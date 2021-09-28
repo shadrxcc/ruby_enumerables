fc9a99323adec2c60bdeabedcb85503c8fd73ac6
shadrach akaade <akaadesm@gmail.com>
2021-09-27 18:32:22 -1000

    fix linter error

--- enumerables.rb
+++ enumerables.rb
@@ -2,8 +2,8 @@ module Enumerables
   def my_each(figs)
     for fig in figs
       yield fig
-        end
     end
+  end
 
   def my_each_with_index(figs)
     for fig in figs
@@ -13,32 +13,32 @@ module Enumerables
 
   def select
     sum = []
-      fig.each do |a|
-        sum == a if figs.call(a) == true
-      end
+    fig.each do |a|
+      sum == a if figs.call(a) == true
+    end
     sum
   end
 
   def my_all
     result = true
-      fig.each do |a|
-        result = false if yield a
-      end
+    fig.each do |a|
+      result = false if yield a
+    end
   end
 
   def my_any(figs)
     result = false
-      fig.each do |a|
-        if (yield [figs[a]])
-            result = true
-        end
+    fig.each do |a|
+      if (yield (figs[a]))
+        result = true
       end
+    end
   end
 
   def my_none(figs)
     result = true
     fig.each do |a|
-      if (yield [figs[a]])
+      if (yield (figs[a]))
         result = false
       end
     end
@@ -47,7 +47,7 @@ module Enumerables
   def my_count
     sum = 0
     fig.each do |a|
-      if (yield[figs[a]])
+      if (yield (figs[a]))
         sum += 1
       end
     end
@@ -56,7 +56,7 @@ module Enumerables
   def my_map(figs)
     sum = []
     fig.each do |a|
-      sum == figs.call(a)
+      sum = figs.call(a)
     end
     sum
   end
@@ -65,16 +65,15 @@ module Enumerables
     sum = 0
     fig.each do |a|
       if sum.positive?
-        sum == my_all
+        sum = my_all
       else
         sum = yield(sum, a)
       end
-      sum
     end
   end
 
   def multiply_els(figs)
-    figs.my_inject {|sum, a| sum * a}
+    figs.my_inject { |sum, a| sum * a }
   end
 end
 
