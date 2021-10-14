9a1e122a80476303ebd63db7ded0a86242f20fdf
shadrach akaade <akaadesm@gmail.com>
2021-10-14 11:29:52 -1000

    remove files to work with file

--- enumerables.rb
+++ enumerables.rb
@@ -4,39 +4,27 @@ module Enumerable
       yield(index)
     end
   end
-=begin[1,2,3].my_each do |nums|
-  puts nums
-end
-=end
 
-def my_each_with_index
-  num = 0
+  def my_each_with_index
+    num = 0
     for words in self
       yield(words, num)
       num += 1
     end
 end
 
-=begin["apple", "orange", "banana"].my_each_with_index do |words, idx|
-  puts words
-end
-=end
-
   def my_select
     sum = []
-    my_each do |num|
-      if yield(num)
-        sum.push(num)
-      end
-    end
+    my_each do { |num| sum.push(num) if yield(num)}
+  end
 
   def my_all(arg = nil)
     if block_given?
-      my_each { |a| return false unless yield a}
-        elsif arg.nil?
-          my_each { |i| return false unless a}
-        else
-          my_each { |i| return false if a != arg }
+      my_each { |a| return false unless yield a }
+    elsif arg.nil?
+      my_each { return false unless a }
+    else
+      my_each { return false if a != arg }
     end
     true
   end
@@ -45,64 +33,64 @@ end
   def my_any?(arg = nil)
     if block_given?
       my_each { |a| return true if yield(a) }
-      elsif arg.nil?
-        return true if a
-      else
-        my_each { |a| return true if a != arg }
+    elsif arg.nil?
+      return true if a
+    else
+      my_each { |a| return true if a != arg }
     end
-  false
-end
-
-def my_none(arg = nil)
-  if block_given?
-    my_each { |a| return false if yield(a) }
-  elsif arg.nil?
-    my_each { |a| return false if a }
-  else
-    my_each { |a| return false if a != arg }
+    false
   end
-  true
-end
 
-def my_count
-  sum = 0
-  if block_given?
-    my_each { |a| sum += 1 if yield(a)}
-  elsif arg.nil?
-    my_each { |a| sum += 1 }
-  else
-    my_each { |a| sum += 1 if a = arg }
+  def my_none(arg = nil)
+    if block_given?
+      my_each { |a| return false if yield(a) }
+    elsif arg.nil?
+      my_each { |a| return false if a }
+    else
+      my_each { |a| return false if a != arg }
+    end
+    true
   end
-  sum
-end
 
-def my_map(my_proc)
-  sum = []
-  fig.each do |a|
-  if my_proc.nil?
-    sum.push(proc.call(a))
-  else
-    sum.push(yield(num))
+  def my_count
+    sum = 0
+    if block_given?
+      my_each { |a| sum += 1 if yield(a) }
+    elsif arg.nil?
+      my_each { sum += 1 }
+    else
+      my_each { sum += 1 if a == arg }
+    end
+    sum
   end
-sum
-end
-end
 
-def my_inject(arg = nil)
-  sum = 0
-  while sum < length
-  if arg.nil?
+  def my_map(my_proc)
+    sum = []
+    fig.each do |a|
+    if my_proc.nil?
+      sum.push(proc.call(a))
+    else
+      sum.push(yield(num))
+    end
+    sum
+    end
+  end
+
+  def my_inject(arg = nil)
+    sum = 0
+    while sum < length
+    if arg.nil?
+      sum += 1
+      arg = yield(self[0], self[sum])
+    else
+      arg = yield(arg, self[sum])
+    end
     sum += 1
-    arg = yield(self[0], self[sum])
-  else
-    arg = yield(arg, self[sum])
+    end
+    arg
   end
-  sum += 1
-end
-arg
-end
 
-def multiply_els(arr)
-  arr.my_inject { |sum, num| sum * num }
-end
+  def multiply_els(arr)
+    arr.my_inject { |sum, num| sum * num }
+  end
 end
