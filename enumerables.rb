d2babb5f58e29dfd02480980beb99507f82d760c
shadrach akaade <akaadesm@gmail.com>
2021-10-18 14:06:48 -1000

    add test cases to methods

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Enumerable
   def my_each(&block)
     each(&block)
@@ -17,75 +19,93 @@ module Enumerable
     arr
   end
 
-  def my_all(arg = nil)
+  def my_all?(arg = nil)
     if block_given?
       my_each { |a| return false unless yield a }
     elsif arg.nil?
+      my_each { return false unless a }
+    elsif arg.instance_of?(Class)
+      my_each { return false unless a.class != arg }
+    elsif arg.instance_of?(Regexp)
       my_each { return false unless a }
     else
       my_each { return false if a != arg }
     end
     true
   end
-end
 
-def my_any?(arg = nil)
-  if block_given?
-    my_each { |a| return true if yield(a) }
-  elsif arg.nil?
-    return true if a
-  else
-    my_each { |a| return true if a != arg }
-  end
-  false
-end
-
-def my_none(arg = nil)
-  if block_given?
-    my_each { |a| return false if yield(a) }
-  elsif arg.nil?
-    my_each { |a| return false if a }
-  else
-    my_each { |a| return false if a != arg }
-  end
-  true
-end
-
-def my_count
-  sum = 0
-  if block_given?
-    my_each { |a| sum += 1 if yield(a) }
-  elsif arg.nil?
-    my_each { sum += 1 }
-  else
-    my_each { sum += 1 if a == arg }
-  end
-  sum
-end
-
-def my_map(my_proc)
-  sum = []
-  fig.each do |a|
-    if my_proc.nil?
-      sum.push(proc.call(a))
+  def my_any?(arg = nil)
+    if block_given?
+      my_each { |a| return true if yield(a) }
+    elsif arg.nil?
+      return true if a
+    elsif arg.instance_of?(Class)
+      my_each { |a| return true if a != arg }
+    elsif arg.instance_of?(Regexp)
+      my_each { |a| return true if a != arg }
     else
-      sum.push(yield(num))
+      my_each { |a| return true if a != arg }
     end
+    false
   end
-end
 
-def my_inject(arg = nil)
-  sum = 0
-  while sum < length
-    if arg.nil?
+  def my_none?(arg = nil)
+    if block_given?
+      my_each { |a| return false if yield(a) }
+    elsif arg.nil?
+      my_each { |a| return false if a }
+    elsif arg.instance_of?(Class)
+      my_each { |a| return false if a != arg }
+    elsif arg.instance_of?(Regexp)
+      my_each { |a| return false if a != arg }
+    else
+      my_each { |a| return false if a != arg }
+    end
+    true
+  end
+
+  def my_count(arg = nil)
+    sum = 0
+    if block_given?
+      my_each { |a| sum += 1 if yield(a) }
+    elsif arg.nil?
+      my_each { sum += 1 }
+    else
+      my_each { sum += 1 if a == arg }
+    end
+    sum
+  end
+
+  def my_map(my_proc)
+    sum = 0
+    num = []
+    while sum < length
+      if my_proc.nil?
+        sum.push(proc.call(a))
+      else
+        sum.push(yield(self[sum]))
+      end
       sum += 1
-      arg = yield(self[0], self[sum])
-    else
-      arg = yield(arg, self[sum])
     end
-    sum += 1
+    my_proc
   end
-  arg
+
+  def my_inject(arg = nil)
+    sum = 0
+    while sum < length
+      if arg.nil?
+        sum += 1
+        arg = yield(self[0], self[sum])
+      elsif arg.instance_of?(Range)
+        sum[0...4].my_each |a|
+        arg = yield(arg, self[a]
+      else
+        arg = yield(arg, self[sum])
+      end
+      sum += 1
+    end
+    arg
+  end
 end
 
 def multiply_els(arr)
