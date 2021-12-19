191762f0a761731ec605b0a47a61a8c50320e37e
shadrach akaade <akaadesm@gmail.com>
2021-12-19 02:37:34 +0100

    test for my_count, my_select and my_all? methods

---
+++ spec/enumerables_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../lib/enumerables'
+
+describe Enumerable do
+  let(:arr) { [4, 5, 8, 2, 1, 9, 3, 7, 6] }
+  describe '#my_count' do
+    it 'return the count of elements that evaluate to true' do
+      expect(arr.my_count { |i| i < 4 }).to eq(3)
+    end
+  end
+
+  describe '#my_select' do
+    it 'should return a particular type of element selected' do
+      expect(arr.my_select(&:even?)).to eql([4, 8, 2, 6])
+    end
+  end
+
+  describe '#my_all' do
+    it 'should return a boolean value true if all the objects in the enumerable satisfies the given condition' do
+      expect(arr.my_all? { |i| i < 4 }).to be false
+    end
+  end
+end
