6bde9d227fddae761f8a9fae72f29d64a45d3978
shadrach akaade <akaadesm@gmail.com>
2021-12-22 20:32:41 +0100

    test for my_each, each_with_index, my_map, my_inject, multiply_els

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,6 +2,19 @@ require_relative '../lib/enumerables'
 
 describe Enumerable do
   let(:arr) { [4, 5, 8, 2, 1, 9, 3, 7, 6] }
+
+  describe '#my_each' do
+    it 'should return each value in the array to the block after iteration' do
+      expect(arr.my_each { |i| puts i }).to eql([4, 5, 8, 2, 1, 9, 3, 7, 6])
+    end
+  end
+
+  describe 'my_each_with_index' do
+    it 'should return indexes of elements in the array' do
+      expect(arr.my_each_with_index { |elem, index| puts "#{index}:#{elem}" }).to eql([4, 5, 8, 2, 1, 9, 3, 7, 6])
+    end
+  end
+
   describe '#my_count' do
     it 'return the count of elements that evaluate to true' do
       expect(arr.my_count { |i| i < 4 }).to eq(3)
@@ -29,6 +42,24 @@ describe Enumerable do
   describe '#my_none?' do
     it 'should return true if no elements are true' do
       expect(arr.my_none? { |i| i > 10 }).to be true
+    end
+  end
+
+  describe '#my_map' do
+    it 'should return double of the numbers in the array' do
+      expect(arr.my_map { |i| i * 2 }).to eql([8, 10, 16, 4, 2, 18, 6, 14, 12])
+    end
+  end
+
+  describe '#my_inject' do
+    it 'returns the sum total of all elements in the array' do
+      expect(arr.my_inject { |sum, num| sum + num }).to eql(45)
+    end
+  end
+
+  describe '#multiply_els' do
+    it 'should mutliply and return the elements in the array' do
+      expect(arr.my_inject { |sum, num| sum * num }).to eql(362_880)
     end
   end
 end
